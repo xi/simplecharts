1b9b88d4c9f25844e871319b06c87a18324da1bc
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-02-20 08:05:53 +0100

    extend setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import find_packages, setup
 
 setup(
     name='simplecharts',
+    description='SVG charts without dependencies',
     version='0.0.1',
     py_modules=['simplecharts'],
+    url='https://github.com/xi/simplecharts',
+    license='GPLv3+',
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Visualization',
+    ]
 )
